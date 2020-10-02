81df838f3eb7ec4bcab6eaf98d15bd766b8795a3
Garima Srivastava <garimas.53@gmail.com>
2020-10-03 02:19:56 +0530

    Api calls using https://github.com/infinitered/apisauce

---
+++ app/constants/AppConstants.js
@@ -0,0 +1,1 @@
+export const API_KEY = '96f6d82be9c2191abdf21a9a354a0db5';
--- app/services/apiService.js
+++ app/services/apiService.js
@@ -1,5 +1,12 @@
 import { setWeather, setError, setIsLoading } from './../actions/weatherAction';
 import Geolocation from '@react-native-community/geolocation';
+import { create } from 'apisauce';
+import { API_KEY } from '../constants/AppConstants';
+
+
+  const api = create({
+    baseURL: 'http://api.openweathermap.org',
+  })
 
 export const getWeather = () => {
     return async (dispatch) => {
@@ -7,6 +14,7 @@ export const getWeather = () => {
             Geolocation.getCurrentPosition(function(location){ 
                 longitude = location.coords.longitude;
                 latitude = location.coords.latitude;
+
                 fetchWeather(dispatch);
             });
             
@@ -14,36 +22,38 @@ export const getWeather = () => {
 }
 
 function fetchWeather(dispatch){
+      
+      api
+        .get(`data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+        .then(response => response.data)
+        .then(function(json){
 
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=96f6d82be9c2191abdf21a9a354a0db5`)
-    .then((response) => response.json())
-    .then(function(json){ 
-        let city = json.city.name;
-        let list = json.list;
-        let todaysWeather = json.list[0];
+            let city = json.city.name;
+            let list = json.list;
+            let todaysWeather = json.list[0];
         
-        let prevDate = todaysWeather.dt_txt.split(" ")[0];
-        todaysWeather["dateText"] = (new Date(prevDate)).toDateString();
-        let weatherList = []
+            let prevDate = todaysWeather.dt_txt.split(" ")[0];
+            todaysWeather["dateText"] = (new Date(prevDate)).toDateString();
+            let weatherList = []
 
-        list.forEach(element => {
-            let currentDate = element.dt_txt.split(" ")[0];
-            if(prevDate == element.dt_txt.replace(element.dt_txt, currentDate)){
-                prevDate = currentDate;
-            }
-            else{
-                let date = new Date(currentDate);
-                element["dateText"] = date.toDateString();
-                weatherList.push(element);
-                prevDate = currentDate;
-            }
-        });
-        
-        dispatch(setWeather({city, weatherList, todaysWeather}));
-    })
-    .catch(function(err){ 
-        console.log(err);
-        dispatch(setError(true));
-    });
+            list.forEach(element => {
+                let currentDate = element.dt_txt.split(" ")[0];
+                if(prevDate == element.dt_txt.replace(element.dt_txt, currentDate)){
+                    prevDate = currentDate;
+                }
+                else{
+                    let date = new Date(currentDate);
+                    element["dateText"] = date.toDateString();
+                    weatherList.push(element);
+                    prevDate = currentDate;
+                }
+            });
+
+            dispatch(setWeather({city, weatherList, todaysWeather}));
+            })
+            .catch(function(err){ 
+                console.log(err);
+                dispatch(setError(true));
+            })
 
 }
